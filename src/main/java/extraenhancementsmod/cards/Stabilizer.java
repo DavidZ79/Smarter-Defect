5cd38a9583171ca3b3db1dd37925ae94263c4559
David Zhou <david.m8f.z@gmail.com>
2023-09-04 07:01:16 -0400

    completed stabilizer functionality

---
+++ src/main/java/extraenhancementsmod/cards/Stabilizer.java
@@ -0,0 +1,54 @@
+package extraenhancementsmod.cards;
+
+import com.megacrit.cardcrawl.actions.defect.AnimateOrbAction;
+import com.megacrit.cardcrawl.actions.defect.EvokeOrbAction;
+import com.megacrit.cardcrawl.actions.defect.EvokeWithoutRemovingOrbAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
+import com.megacrit.cardcrawl.orbs.Dark;
+import extraenhancementsmod.util.CardStats;
+
+public class Stabilizer extends BaseCard {
+    public static final String ID = makeID("Stabilizer");
+    private static final CardStats info = new CardStats(
+            AbstractCard.CardColor.BLUE,
+            CardType.SKILL,
+            CardRarity.UNCOMMON,
+            CardTarget.NONE,
+            1
+    );
+
+    public Stabilizer() {
+        super(ID, info);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) { // dualcast lol
+        this.addToBot(new AnimateOrbAction(1));
+        this.addToBot(new EvokeWithoutRemovingOrbAction(1));
+        this.addToBot(new AnimateOrbAction(1));
+        this.addToBot(new EvokeOrbAction(1));
+        if (this.upgraded) {
+            this.addToBot(new AnimateOrbAction(1));
+            this.addToBot(new EvokeWithoutRemovingOrbAction(1));
+            this.addToBot(new AnimateOrbAction(1));
+            this.addToBot(new EvokeOrbAction(1));
+        }
+    }
+
+
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgraded = true;
+            this.rawDescription = cardStrings.UPGRADE_DESCRIPTION;
+            this.initializeDescription();
+        }
+    }
+}
+
+
