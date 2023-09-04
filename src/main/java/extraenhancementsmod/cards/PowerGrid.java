82610d10a0d7c51fa0501b8821b73e668f0fa4df
David Zhou <david.m8f.z@gmail.com>
2023-09-03 22:49:35 -0400

    finished power grid functionality

--- src/main/java/extraenhancementsmod/cards/PowerGrid.java
+++ src/main/java/extraenhancementsmod/cards/PowerGrid.java
@@ -19,6 +19,8 @@ public class PowerGrid extends BaseCard {
 
     public PowerGrid() {
         super(ID, info);
+        this.baseMagicNumber = 2;
+        this.magicNumber = baseMagicNumber;
     }
 
     @Override
