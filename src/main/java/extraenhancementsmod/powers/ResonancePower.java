82f8f6228d68662fb4f030c6320696bb27ef7573
David Zhou <david.m8f.z@gmail.com>
2023-09-04 04:01:26 -0400

    removed an unused import

--- src/main/java/extraenhancementsmod/powers/ResonancePower.java
+++ src/main/java/extraenhancementsmod/powers/ResonancePower.java
@@ -9,7 +9,6 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.orbs.AbstractOrb;
 import com.megacrit.cardcrawl.orbs.EmptyOrbSlot;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.vfx.ThoughtBubble;
 
 import static extraenhancementsmod.BasicMod.makeID;
 
