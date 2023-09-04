6c3e9e7d1d49597caac3299c7d07b96473c67beb
David Zhou <david.m8f.z@gmail.com>
2023-09-04 03:56:59 -0400

    removed an unused import

--- src/main/java/extraenhancementsmod/powers/ResonancePower.java
+++ src/main/java/extraenhancementsmod/powers/ResonancePower.java
@@ -2,7 +2,6 @@ package extraenhancementsmod.powers;
 
 import basemod.interfaces.CloneablePowerInterface;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.DamageRandomEnemyAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
