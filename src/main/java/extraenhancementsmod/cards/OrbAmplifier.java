033642c83d44ac427fdfe74038e4c82e469781c9
David Zhou <david.m8f.z@gmail.com>
2023-09-04 21:53:00 -0400

    completed orb amp functionality

---
+++ src/main/java/extraenhancementsmod/cards/OrbAmplifier.java
@@ -0,0 +1,59 @@
+package extraenhancementsmod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.defect.AnimateOrbAction;
+import com.megacrit.cardcrawl.actions.defect.EvokeOrbAction;
+import com.megacrit.cardcrawl.actions.defect.EvokeWithoutRemovingOrbAction;
+import com.megacrit.cardcrawl.actions.defect.IncreaseMaxOrbAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
+import com.megacrit.cardcrawl.orbs.Dark;
+import com.megacrit.cardcrawl.orbs.EmptyOrbSlot;
+import extraenhancementsmod.util.CardStats;
+
+public class OrbAmplifier extends BaseCard {
+    public static final String ID = makeID("OrbAmplifier");
+    private static final CardStats info = new CardStats(
+            AbstractCard.CardColor.BLUE,
+            CardType.POWER,
+            CardRarity.UNCOMMON,
+            CardTarget.SELF,
+            1
+    );
+
+    public OrbAmplifier() {
+        super(ID, info);
+    }
+
+    @Override
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int orbs = 0;
+        for(int i = 0; i < p.orbs.size(); ++i) {
+            if (!(p.orbs.get(i) instanceof EmptyOrbSlot)) {
+                orbs++;
+            }
+        }
+        this.addToBot(new IncreaseMaxOrbAction(orbs));
+        if (this.upgraded) {
+            this.addToBot(new IncreaseMaxOrbAction(2));
+        }
+    }
+
+
+    public void upgrade() {
+        if (!this.upgraded) {
+            this.upgradeName();
+            this.upgraded = true;
+            this.rawDescription = cardStrings.UPGRADE_DESCRIPTION;
+            this.initializeDescription();
+        }
+    }
+}
+
+
