f6d31886236966467665bb569485add506279877
David Zhou <david.m8f.z@gmail.com>
2023-10-02 14:23:44 -0400

    Fixed ZeroPoint bug, TODO: test

--- src/main/java/extraenhancementsmod/powers/LoseFocusPower.java
+++ src/main/java/extraenhancementsmod/powers/LoseFocusPower.java
@@ -1,32 +1,27 @@
 package extraenhancementsmod.powers;
 
 import basemod.interfaces.CloneablePowerInterface;
-import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.orbs.AbstractOrb;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.FocusPower;
 import static extraenhancementsmod.BasicMod.makeID;
 
-public class ZeroPointPower extends BasePower implements CloneablePowerInterface {
-    public static final String POWER_ID = makeID("ZeroPointPower");
+public class LoseFocusPower extends BasePower implements CloneablePowerInterface {
+    public static final String POWER_ID = makeID("LoseFocusPower");
     private static final AbstractPower.PowerType TYPE = AbstractPower.PowerType.BUFF;
     private static final boolean TURN_BASED = false;
 
-    public ZeroPointPower(AbstractCreature owner, int amount) {
+    public LoseFocusPower(AbstractCreature owner, int amount) {
         super(POWER_ID, TYPE, TURN_BASED, owner, amount);
     }
 
     @Override
-    public void onChannel(AbstractOrb orb) {
-        if (orb.ID.equals("Frost")) {
-
-            // gain 1 focus per frost
-            addToBot(new ApplyPowerAction(owner, owner, new FocusPower(owner, this.amount)));
-
-            // lose 1 focus per frost
-            addToBot(new ApplyPowerAction(owner, owner, new LoseFocusPower(owner, this.amount), this.amount));
-        }
+    public void atEndOfTurn(boolean isPlayer) {
+        this.flash(); // see LoseStrengthPower from Flex
+        addToBot(new ApplyPowerAction(this.owner, this.owner, new FocusPower(this.owner, -this.amount), -this.amount));
+        this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, "ZeroPoint")); // or ZeroPointPower?
     }
 
     public void updateDescription() {
